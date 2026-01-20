54b77468c97b233e9b833e7bf4f331f8cc9e2bb1
ngushteraklieva <ngushteraklieva@gmail.com>
2026-01-20 12:27:36 +0100

    Create Account Controller

--- API.Utility/SD.cs
+++ API.Utility/SD.cs
@@ -2,5 +2,9 @@
 {
     public static class SD
     {
+        //regex
+        public const string UserNameRegex = "^[]a-zA-z0-9_.-]*$";
+        public const string EmailRegex = "^.+@[^\\.].*\\.[a-z]{2,}$";
+
     }
 }
---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using API.DTOs.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+    }
+}
